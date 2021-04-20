a4673d7abd38ee8a2a2857a30fdf1152c59d4c71
Hannah Buckland <hannah.buckland@bristol.ac.uk>
2021-04-20 12:18:43 +0100

    netflix titles stipe plot

---
+++ 2021-04-20/netflix_titles.R
@@ -0,0 +1,90 @@
+# TidyTuesday 19/04/2021
+# Hannah Buckland
+# Netflix titles - data from the Kaggle
+
+library(tidytuesdayR)
+library(tidyverse)
+library(ggthemes)
+library(patchwork)
+library(extrafont)
+library(ggtext)
+library(ggfx)
+library(lubridate)
+library(Cairo)
+
+tuesdata <- tidytuesdayR::tt_load('2021-04-20') # read in TidyTuesday Datat
+
+# Convert date added to date using lubridate and filter from 2015 onwards
+netflix <- tuesdata$netflix_titles %>%
+  mutate(date_add_parse = mdy(date_added),
+         date_add_parse = date(date_add_parse),
+         date_month = floor_date(date_add_parse,unit = "month")) %>%
+  filter(date_month >= "2015-01-01") 
+
+# Count the number of titles added every month
+monthly <- netflix %>%
+  count(date_month) %>%
+  mutate(label_pos = as.numeric(date_month))
+
+# Plotting -----------
+
+# x axis breaks and labels
+axisbreaks <- monthly$label_pos[seq(1,length(monthly$label_pos),12)]
+
+axislabels <- data.frame(label = monthly$date_month[seq(1,length(monthly$date_month),12)]) %>%
+  mutate(label = str_sub(label,end=4))
+
+# colour information
+bgcolour <- "grey10"
+txtcolour <- "grey60"
+
+# horizontal tile plot
+stripplt <- ggplot() + 
+  as_reference(
+    geom_text(aes(x=16500, y=1.2, label = 'NETFLIX'), hjust = 0,size = 30, family = "Bebas Neue"),
+    id = "text"
+  ) +
+  with_blend(geom_tile(data = monthly, 
+                       mapping = aes(x=date_month,
+                                     y=1,
+                                     fill = n),width = 30),
+             bg_layer = "text",
+             blend_type = 'out',
+             id = "blended"
+  ) +
+  with_shadow("blended", sigma = 3, colour = "black") +
+  scale_fill_gradient(low =  "#e50914", high ="#190103") +
+  theme_void() +
+  scale_x_continuous(breaks = axisbreaks, 
+                     labels = axislabels$label, 
+                     position = "bottom") +
+  labs(caption = "Visualisation: Hannah Buckland | @HannahMBuckland \n Source: Data from Kaggle | #TidyTuesday \n") +
+  guides(fill = guide_colourbar(
+    barwidth = 15,
+    barheight = 0.5,
+    frame.colour = txtcolour,
+    ticks = TRUE,
+    ticks.colour = txtcolour,
+    ticks.linewidth = 1,
+    title = "Number of Titles Added Per Month",
+    title.hjust = 0.5,
+    title.position = "bottom"
+  )) +
+  theme(axis.text.x = element_text(family = "Bebas Neue",
+                                   color = txtcolour,
+                                   size = 12),
+        plot.background = element_rect(fill = bgcolour),
+        legend.position = "bottom",
+        legend.box.margin = margin(1,0,0,0,"cm"),
+        text = element_text(family = "Bebas Neue",
+                            color = txtcolour),
+        plot.margin = margin(1,0.5,0,0.5,"cm"),
+        plot.caption = element_text(family = "Raleway", color = txtcolour, 
+                                    size = 8, hjust = 1,
+                                    margin = margin(t = 30)))
+
+stripplt
+
+#save as Cairo png (anti alias)
+ggsave("2021-04-20/netflix.png", stripplt, width = 8, height = 5, dpi = 300, type = "cairo-png")
+
